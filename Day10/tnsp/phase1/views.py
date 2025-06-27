86568d287190762faba0bfd4884780820f3febf7
saddalabhavani <saddalabhavani@gmail.com>
2025-06-27 16:05:43 +0530

    Add files via upload

---
+++ Day10/tnsp/phase1/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import  loader
+def  day1(request):
+    temp=loader.get_template('day1.html')
+    return HttpResponse(temp.render())
+def  day2(request):
+    temp=loader.get_template('day2.html')
+    return HttpResponse(temp.render())
+def  day3(request):
+    temp=loader.get_template('day3.html')
+    return HttpResponse(temp.render())
+def  day4(request):
+    temp=loader.get_template('day4.html')
+    return HttpResponse(temp.render())
+def  day5(request):
+    temp=loader.get_template('day5.html')
+    return HttpResponse(temp.render())
+def  day6(request):
+    temp=loader.get_template('day6.html')
+    return HttpResponse(temp.render())
+def  day7(request):
+    temp=loader.get_template('day7.html')
+    return HttpResponse(temp.render())
+def  day8(request):
+    temp=loader.get_template('day8.html')
+    return HttpResponse(temp.render())
+def  day9(request):
+    temp=loader.get_template('day9.html')
+    return HttpResponse(temp.render())
+def  day10(request):
+    temp=loader.get_template('day10.html')
+    return HttpResponse(temp.render())
+def  day11(request):
+    temp=loader.get_template('day11.html')
+    return HttpResponse(temp.render())
+def  day12(request):
+    temp=loader.get_template('day12.html')
+    return HttpResponse(temp.render())
+def  day13(request):
+    temp=loader.get_template('day13.html')
+    return HttpResponse(temp.render())
+def  day14(request):
+    temp=loader.get_template('day14.html')
+    return HttpResponse(temp.render())
+def  day15(request):
+    temp=loader.get_template('day15.html')
+    return HttpResponse(temp.render())
+
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Create your views here.

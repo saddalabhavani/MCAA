426767e25739666fd870e3972698404c9f282138
saddalabhavani <saddalabhavani@gmail.com>
2025-06-28 10:55:53 +0530

    Add files via upload

---
+++ 1/myproject/sem/views.py
@@ -0,0 +1,106 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+def sems(request):
+   temp=loader.get_template('sems.html')
+   return HttpResponse(temp.render())
+
+def mca101(request):
+   temp=loader.get_template('mca101.html')
+   return HttpResponse(temp.render())
+def mca102(request):
+   temp=loader.get_template('mca102.html')
+   return HttpResponse(temp.render())
+def mca103(request):
+   temp=loader.get_template('mca103.html')
+   return HttpResponse(temp.render())
+
+def mca104(request):
+   temp=loader.get_template('mca104.html')
+   return HttpResponse(temp.render())
+
+def mca105(request):
+   temp=loader.get_template('mca105.html')
+   return HttpResponse(temp.render())
+
+def mca106(request):
+   temp=loader.get_template('mca106.html')
+   return HttpResponse(temp.render())
+def mca107(request):
+   temp=loader.get_template('mca107.html')
+   return HttpResponse(temp.render())
+def mca108(request):
+   temp=loader.get_template('mca108.html')
+   return HttpResponse(temp.render())
+def mca201(request):
+   temp=loader.get_template('mca201.html')
+   return HttpResponse(temp.render())
+def mca202(request):
+   temp=loader.get_template('mca202.html')
+   return HttpResponse(temp.render())
+def mca203(request):
+   temp=loader.get_template('mca203.html')
+   return HttpResponse(temp.render())
+def mca204(request):
+   temp=loader.get_template('mca204.html')
+   return HttpResponse(temp.render())
+
+def mca205(request):
+   temp=loader.get_template('mca205.html')
+   return HttpResponse(temp.render())
+
+def mca206(request):
+   temp=loader.get_template('mca206.html')
+   return HttpResponse(temp.render())
+
+def mca207(request):
+   temp=loader.get_template('mca207.html')
+   return HttpResponse(temp.render())
+def mca208(request):
+   temp=loader.get_template('mca208.html')
+   return HttpResponse(temp.render())
+def mca301(request):
+   temp=loader.get_template('mca301.html')
+   return HttpResponse(temp.render())
+def mca302(request):
+   temp=loader.get_template('mca302.html')
+   return HttpResponse(temp.render())
+def mca303(request):
+   temp=loader.get_template('mca303.html')
+   return HttpResponse(temp.render())
+def mca304(request):
+   temp=loader.get_template('mca304.html')
+   return HttpResponse(temp.render())
+def mca305(request):
+   temp=loader.get_template('mca305.html')
+   return HttpResponse(temp.render())
+
+def mca306(request):
+   temp=loader.get_template('mca306.html')
+   return HttpResponse(temp.render())
+
+def mca307(request):
+   temp=loader.get_template('mca307.html')
+   return HttpResponse(temp.render())
+
+def mca308(request):
+   temp=loader.get_template('mca308.html')
+   return HttpResponse(temp.render())
+
+def mca309(request):
+   temp=loader.get_template('mca309.html')
+   return HttpResponse(temp.render())
+
+def mca401(request):
+   temp=loader.get_template('mca401.html')
+   return HttpResponse(temp.render())
+
+def mca402(request):
+   temp=loader.get_template('mca402.html')
+   return HttpResponse(temp.render())
+def mca403(request):
+   temp=loader.get_template('mca403.html')
+   return HttpResponse(temp.render())
+
+
+# Create your views here.
